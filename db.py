05d5e0ed9039db7b464b6f2da91907ec5d000ae8
liupeng <1269861316@qq.com>
2025-01-17 11:13:06 +0800

    fixed

--- db.py
+++ db.py
@@ -52,7 +52,7 @@ class User(BaseModel):
     # nullable是否为空，unique唯一约束
     user_id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), nullable=False, unique=True)
-    password = db.Column(db.String(150), nullable=True)
+    password = db.Column(db.String(150), nullable=False)
     nickname = db.Column(db.String(50), nullable=True)
     avatar = db.Column(db.String(50), nullable=True)
     qq = db.Column(db.String(100), nullable=True)
